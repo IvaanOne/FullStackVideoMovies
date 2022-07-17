1e484ab3c55bfc58e4fb43be5e745cf581872f5d
IvaanOne <ivaanone17@gmail.com>
2022-07-17 19:34:26 +0200

    feat: register design and route

---
+++ src/containers/User/Register/Register.js
@@ -0,0 +1,147 @@
+
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom'
+
+const Register = (props) => {
+
+    //Hooks
+
+    const [datosUser, setDatosUser] = useState({
+        name: "",
+        surname: "",
+        email: "",
+        password: "",
+        password2: "",
+        phone: "",
+        adress: "",
+        birth: ""
+    });
+
+    const [msgError, setMsgError] = useState("");
+
+    const [congratulations, setCongratulations] = useState("");
+
+    //Variables
+    let navigate = useNavigate();
+
+    //Handlers
+    const updateUserData = (e) => {
+        setDatosUser({ ...datosUser, [e.target.name]: e.target.value })
+    }
+
+    //Funciones del componente (useEffect)
+
+    useEffect(() => {
+        //UseEffect equivalente a componentDidMount (montado)
+
+    }, [])
+
+    useEffect(() => {
+        //UseEffect equivalente a componentDidUpdate (actualizado)
+
+    })
+
+    //Funciones
+
+    const Registrame = async () => {
+
+        //Primero, comprobación de campos vacíos
+
+        let mandatory = ['name', 'surname', 'email', 'password', 'password2', 'phone', 'adress', 'birth'];
+
+        for (let field of mandatory) {
+            if (datosUser[field] === '') {
+                setMsgError(`Te ha faltado ${[field]} por rellenar`);
+                return;
+            }
+        }
+
+        //Comprobamos que el password esté repetido correctamente
+
+        if (datosUser.password !== datosUser.password2) {
+            setMsgError("Los dos password deben de coincidir");
+            return;
+        }
+
+        //Comprobaciones de formato de datos...
+
+
+        //Limpiamos error
+        setMsgError(false);
+
+        // try {
+
+        //     //axios....comunicamos con el backend
+
+        //     // let resultado = await axios.post("endpointregister",datosUser);
+
+        setCongratulations(true);
+
+        setTimeout(() => {
+            navigate("/login");
+        }, 2000);
+        // } catch (error){
+        //     console.log(error)
+        // }
+    }
+
+
+    if (congratulations === true) {
+
+        return (
+            <div className="registerDesign">
+                Bienvenido a nuestra aplicación, {datosUser.name}....
+            </div>
+        )
+
+    } else {
+        return (
+            <html>
+                <body class="main-bg">
+                    <div class="login-container text-c animated flipInX">
+                        <div>
+                            <h1 class="logo-badge text-whitesmoke"><span class="fa fa-user-circle"></span></h1>
+                        </div>
+                        <h3 class="text-whitesmoke">Regístrate en nuestra web!</h3>
+                        <p class="text-whitesmoke"></p>
+                        <div class="container-content">
+                            <form class="margin-t">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="Nombre" required=""></input>
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="Apellido" required=""></input>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" placeholder="Email" required=""></input>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" class="form-control" placeholder="Contraseña" required=""></input>
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="Repetir contraseña" required=""></input>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" placeholder="Teléfono" required=""></input>
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" placeholder="Direccion" required=""></input>
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" placeholder="Fecha nacimiento Mes/Dia/Año XX/XX/XXXX" required=""></input>
+                                    </div>
+                                </div>
+                                <button type="submit" class="form-button button-l margin-b registerButon" onClick={() => Registrame()}>Registrar</button>
+
+                                <a class="text-darkyellow" href="#"><small>{msgError}</small></a>
+                            </form>
+                        </div>
+                    </div>
+                </body>
+            </html>
+        )
+    }
+
+}
+export default Register;
